842081f2ab868ca90171a9f4d28b19f1bcc9d686
Samuelson Okoi <samuelsonokoi@gmail.com>
2021-07-25 05:14:50 +0100

    added application.hbs and yeild tag

--- app/components/jumbo.js
+++ app/components/jumbo.js
@@ -1,6 +1,3 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 
-export default class JumboComponent extends Component {
-  @tracked name = 'Ember';
-}
+export default class JumboComponent extends Component { }
