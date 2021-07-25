ab9774a06f0d609d57ea23dccdada5d65fbde04f
Samuelson Okoi <samuelsonokoi@gmail.com>
2021-07-25 05:02:33 +0100

    added navbar component and added test for nav-bar

--- tests/integration/components/nav-bar-test.js
+++ tests/integration/components/nav-bar-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | people-list', function (hooks) {
+module('Integration | Component | nav-bar', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<PeopleList />`);
+    await render(hbs`<NavBar />`);
 
     assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
-      <PeopleList>
+      <NavBar>
         template block text
-      </PeopleList>
+      </NavBar>
     `);
 
     assert.dom(this.element).hasText('template block text');
